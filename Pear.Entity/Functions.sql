3005b9287860032476d56bd3a2f6ef5d975646c6
seraphau <seraphau@gmail.com>
2015-02-17 12:05:15 +1100

    Added Functions.sql to Pear.Entity

---
+++ Pear.Entity/Functions.sql
@@ -0,0 +1,24 @@
+﻿/****** Object:  UserDefinedFunction [dbo].[fnGetStoresByDistance]    Script Date: 2/17/2015 12:03:27 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER FUNCTION [dbo].[fnGetStoresByDistance] 
+(
+	@latitude real,
+	@longitude real,
+	@maxDistance real
+)
+RETURNS TABLE 
+AS
+RETURN 
+(
+	select *
+	from Store
+	where geography::STGeomFromText('POINT(' + convert(varchar(50), @latitude) + ' ' + convert(varchar(50), @longitude) + ')', 4326).STDistance(geography::STGeomFromText('POINT(' + convert(varchar(50), Latitude) + ' ' + convert(varchar(50), Longitude) + ')', 4326)) <= @maxDistance
+)
